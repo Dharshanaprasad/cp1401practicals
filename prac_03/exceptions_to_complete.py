362a8050b7dbcc63119b42dfda68e044d63cf9b6
Dharshanaprasad <dharshanasimran.prasad@myjcu.edu.au>
2024-10-06 22:36:54 +1000

    Complete exception handling for safe integer input

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,9 @@
+# Program that safely gets an integer from the user
+is_valid_input = False
+while not is_valid_input:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_valid_input = True
+    except ValueError:
+        print("Invalid input; please enter a valid integer.")
+print(f"You entered: {result}")
